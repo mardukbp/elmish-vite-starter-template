0d4e45dc808af736a41d6e19f0375e5d8a54f9e0
Marduk Bolaños <mardukbp@mac.com>
2025-04-23 14:13:45 +0200

    Updated Font Awesome to Version 5

--- src/App.fs
+++ src/App.fs
@@ -11,8 +11,7 @@ open type prop
 
 type Bulma = CssClasses<"https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css", Naming.PascalCase>
 
-type FA =
-    CssClasses<"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", Naming.PascalCase>
+type FA = CssClasses<"https://cdn.jsdelivr.net/npm/fontawesome5-fullcss@1.1.0/css/all.min.css", Naming.PascalCase>
 
 type Todo =
     { Id: Guid
